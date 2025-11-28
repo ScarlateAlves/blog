32738cd5c128322d0a1943e373ec1f1933245bd7
ScarlateAlves <scarlate100@gmail.com>
2025-11-28 07:12:14 -0300

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
           height="500"
         />
         <div className="max-w-4xl flex flex-col justify-between items-center gap-5 text-center">
-          <Heading>Best place to travel to in Spring</Heading>
+          <Heading>Eu eita teste teste</Heading>
           <Text>
             coisa Lorem ipsum dolor sit amet consectetur. Non egestas a quisque
             odio in. Sed sit lacus in nisi proin eu ut. Quis nunc eget aliquam
